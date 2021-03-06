1cc66152c2878df2ab38ac8088eef201209b1bc6
Liz Lam <lizlam.217@gmail.com>
2021-03-06 15:58:31 -0800

    Add background.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e2ce4f13b87dd8a76aa3";
+/******/ 	var hotCurrentHash = "6bf01cf2065513d65e4c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
+import Head from 'next/head'
+
 function Home() {
   return (
-    <div>a gentle introduction to git</div>
+    <>
+      <Head>
+        <title>a gentle intro</title>
+      </Head>
+      <div>a gentle introduction to git</div>
+      <progress value=".2" max="1.0"></progress>
+    </>
   )
 }
 
